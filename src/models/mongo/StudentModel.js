a19e1430b128fe965bbe7620889695e4067ea3b8
farzal <farzal.khan@grotabyte.com>
2020-11-15 17:03:08 +0500

    student schema modified

--- src/models/mongo/StudentModel.js
+++ src/models/mongo/StudentModel.js
@@ -6,9 +6,28 @@ const { Schema } = db.secondary;
 const studentSchema = new Schema({
     name: String,
     email: String,
-    roll_no: String,
+    roll_no: Number,
     comments: String,
     institute: String,
+    grade: String,
+    city: String,
+    fees: Number,
+    state: String,
+    zip: String,
+    address: String,
+    phone: String,
+    department: String,
+    category_id: Number,
+    courses: [{
+        title: String,
+        instructor: String
+    }],
+    finance: {
+        account: String, 
+        accountName: String,
+        creditCardNumber: String,
+        iban: String
+    },
     createdAt : { type : Date, default: Date.now }
 });
 
